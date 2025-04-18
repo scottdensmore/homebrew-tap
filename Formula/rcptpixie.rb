4dcba776ca35194ccc49f18dfca1449b7b74ba60
Scott Densmore <scottdensmore@mac.com>
2025-04-17 20:25:26 -0700

    Update rcptpixie formula with correct SHA256

--- Formula/rcptpixie.rb
+++ Formula/rcptpixie.rb
@@ -2,7 +2,7 @@ class Rcptpixie < Formula
   desc "A command-line tool that automatically renames PDF receipts using AI-powered text extraction"
   homepage "https://github.com/scottdensmore/rcptpixie"
   url "https://github.com/scottdensmore/rcptpixie/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "PLACEHOLDER_SHA256" # This will be updated after the first release
+  sha256 "eae3fc78468698393e6f13f1f2f1ec892cf8cf5c63724d14fe73fb50aa0c75b2"
   license "MIT"
 
   depends_on "go" => :build
